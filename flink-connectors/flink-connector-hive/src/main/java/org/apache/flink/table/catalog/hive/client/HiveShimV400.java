6ac11a30eda490ea312f785571860bcd5574a350
Shubham Sharma <shubh.luck@yahoo.in>
2024-09-05 00:11:41 -0400

    ODP-7047: ODP-2054 Flink Hive4.0.0 fixes & integrations

--- flink-connectors/flink-connector-hive/src/main/java/org/apache/flink/table/catalog/hive/client/HiveShimV400.java
+++ flink-connectors/flink-connector-hive/src/main/java/org/apache/flink/table/catalog/hive/client/HiveShimV400.java
@@ -18,5 +18,55 @@
 
 package org.apache.flink.table.catalog.hive.client;
 
+import org.apache.flink.table.catalog.exceptions.CatalogException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.ql.exec.FileSinkOperator;
+import org.apache.hadoop.hive.ql.io.HiveOutputFormat;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.util.Progressable;
+
+import java.lang.reflect.Method;
+import java.util.Properties;
+
 /** Shim for Hive version 4.0.0. */
-public class HiveShimV400 extends HiveShimV313 {}
+public class HiveShimV400 extends HiveShimV313 {
+
+    public FileSinkOperator.RecordWriter getHiveRecordWriter(
+            JobConf jobConf,
+            Class outputFormatClz,
+            Class<? extends Writable> outValClz,
+            boolean isCompressed,
+            Properties tableProps,
+            Path outPath) {
+        try {
+            HiveOutputFormat<?, ?> hiveOutputFormat =
+                    (HiveOutputFormat<?, ?>) outputFormatClz.newInstance();
+            Class<? extends HiveOutputFormat> utilClass = hiveOutputFormat.getClass();
+            Method utilMethod =
+                    utilClass.getDeclaredMethod(
+                            "getHiveRecordWriter",
+                            JobConf.class,
+                            Path.class,
+                            Class.class,
+                            boolean.class,
+                            Properties.class,
+                            Progressable.class);
+
+            return (FileSinkOperator.RecordWriter)
+                    utilMethod.invoke(
+                            hiveOutputFormat,
+                            jobConf,
+                            outPath,
+                            outValClz,
+                            isCompressed,
+                            tableProps,
+                            Reporter.NULL);
+
+        } catch (Exception e) {
+            throw new CatalogException("Failed to create Hive RecordWriter", e);
+        }
+    }
+}
