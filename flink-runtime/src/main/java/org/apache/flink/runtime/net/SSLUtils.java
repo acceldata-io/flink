b2f48722018f502de489efd4d0d06526000fb290
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2026-08-27 01:27:31 -0400

    ODP-5754 Failed to decrypt obfuscated password as missing to decrypt … (#17)
    * ODP-5754 Failed to decrypt obfuscated password as missing to decrypt truststore password
    
    * ODP-5754 Fixed PekkoUtils to decrypt obfuscated passwords

--- flink-runtime/src/main/java/org/apache/flink/runtime/net/SSLUtils.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/net/SSLUtils.java
@@ -244,7 +244,7 @@ public class SSLUtils {
         KeyStore trustStore = KeyStore.getInstance(truststoreType);
         try (InputStream trustStoreFile =
                 Files.newInputStream(new File(trustStoreFilePath).toPath())) {
-            trustStore.load(trustStoreFile, trustStorePassword.toCharArray());
+            trustStore.load(trustStoreFile, decryptPassword(trustStorePassword).toCharArray());
         }
 
         String certFingerprint =
